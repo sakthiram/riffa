c97f16c3541508e5d8f525949b5d2add63b3149d
Sakthi Sivaraman <srsivara@ucsd.edu>
2016-09-19 13:22:22 -0700

    Removed unwanted files from rtl

--- riffa_tb/rx_tx_port_tb/rtl/work/syncff/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity syncff is
-    port(
-        CLK             : in     vl_logic;
-        IN_ASYNC        : in     vl_logic;
-        OUT_SYNC        : out    vl_logic
-    );
-end syncff;
